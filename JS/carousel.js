9825108f0b83dadc679977e92d948ef47052497c
Miguel Simões <55927203+SirMengo@users.noreply.github.com>
2021-01-24 20:49:22 +0000

    Hopefully Versão Final

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,26 +1,30 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+MostraSlide(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+/* Funçao que avança */
+function avaSlide(n) {
+  MostraSlide(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+/* Slide atual */
+function atualSlide(n) {
+  MostraSlide(slideIndex = n);
 }
 
-function showSlides(n) {
+/*Descobre aonde o carousel se encontra de momento, dependendo do input do utilizador avança ou
+volta atrás um slide, atualiza tbm os pontos (debaixo da imagem) */
+function MostraSlide(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("meuSlide");
+  var pontos = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < pontos.length; i++) {
+      pontos[i].className = pontos[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  pontos[slideIndex-1].className += " active";
 }
---
+++ JS/verificar.js
@@ -0,0 +1,62 @@
+function verificar(){
+    if(document.getElementById("Nome").value== "" || document.getElementById("Nome").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Nome").value== "" || document.getElementById("Nome").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("NIF").value== "" || document.getElementById("NIF").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Tlm").value== "" || document.getElementById("Tlm").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Rua").value== "" || document.getElementById("Rua").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Num").value== "" || document.getElementById("Num").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Postal").value== "" || document.getElementById("Postal").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Cidade").value== "" || document.getElementById("Cidade").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Pais").value== "" || document.getElementById("Pais").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Distrito").value== "" || document.getElementById("Distrito").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Exp").value== "" || document.getElementById("Exp").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("Pais").value== "" || document.getElementById("Pais").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else if(document.getElementById("CVV").value== "" || document.getElementById("CVV").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+    
+    else if(document.getElementById("Prop").value== "" || document.getElementById("Prop").value== undefined){
+        alert("Um ou mais campos obrigatórios vazios!");
+    }
+
+    else{
+        location.replace("../carrinho/recibo.html");
+    }
+    
+}
